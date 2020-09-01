56aa15852b6870a269da2982897d50a7a19aa4ad
bg-multimedia <48929583+bg-multimedia@users.noreply.github.com>
2020-09-01 16:22:22 -0300

    FIX

--- src/main/java/com/xmen/api/mutant/Mutant.java
+++ src/main/java/com/xmen/api/mutant/Mutant.java
@@ -6,12 +6,20 @@ public class Mutant{
 
     private static final int seqLength = 4;
     private static int found = 0;
+    private static String regex = "[ATGC]*";
+
+    enum Direction {
+        HORIZONTAL,
+        VERTICAL,
+        LEFT_RIGHT,
+        RIGHT_LEFT
+    }
 
     public static boolean isValidDNA(String[] input) {
         if (input.length < seqLength){
             return false;
         }
-        Pattern validCharacterPattern = Pattern.compile("[ATGC]*");
+        Pattern validCharacterPattern = Pattern.compile(regex);
         Pattern validLengthPattern = Pattern.compile(".{" + input.length + "}");
         for (int i = 0; i < input.length; i++) {
             if (!validCharacterPattern.matcher(input[i]).matches() || !validLengthPattern.matcher(input[i]).matches())
@@ -25,10 +33,10 @@ public class Mutant{
 
         found = 0;
         
-        if(searchHorizontalVertical(dna.getDna(), "HORIZONTAL") ||
-            searchHorizontalVertical(dna.getDna(), "VERTICAL") ||
-                    searchDiagonal(dna.getDna(), "1") || 
-                    searchDiagonal(dna.getDna(), "2")
+        if(searchHorizontalVertical(dna.getDna(), Direction.HORIZONTAL) ||
+            searchHorizontalVertical(dna.getDna(), Direction.VERTICAL) ||
+                    searchDiagonal(dna.getDna(), Direction.LEFT_RIGHT) || 
+                    searchDiagonal(dna.getDna(), Direction.RIGHT_LEFT)
             ){
                 return true;
             }else{
@@ -38,18 +46,18 @@ public class Mutant{
     }
 
 
-    private static boolean searchHorizontalVertical(String[] dna, String direction) {
+    private static boolean searchHorizontalVertical(String[] dna, Direction direction) {
         String newSeq = "";
         int missingChars = seqLength;
         char currentChar = '.';
         char nextChar = '.';
         for (int i = 0; i < dna.length; i++) {
              for (int j = 0; j + missingChars <= dna.length; j++) {
-                if (direction.equals("HORIZONTAL")) {
+                if (direction == Direction.HORIZONTAL) {
                     currentChar = dna[i].charAt(j);
                     nextChar = dna[i].charAt(j+1);
                 }
-                if (direction.equals("VERTICAL")) {
+                if (direction == Direction.VERTICAL) {
                     currentChar = dna[j].charAt(i);
                     nextChar = dna[j+1].charAt(i);
                 }
@@ -61,7 +69,7 @@ public class Mutant{
         return false;
     }
 
-       private static boolean searchDiagonal(String[] dna, String direction) {
+       private static boolean searchDiagonal(String[] dna, Direction direction) {
         int missingChars = seqLength;
         String newSeq = "";
 
@@ -69,7 +77,7 @@ public class Mutant{
         char nextChar = '.';
         // read first and last column
         int aux = 0;
-        if (direction.equals("2"))
+        if (direction == Direction.RIGHT_LEFT)
             aux = dna.length - 1;// FROM RIGTH
         for (int i = seqLength - 1, j = aux; i < dna.length; i++) {
             for (int a = i, b = j; a - missingChars + 1 >= 0; a--) {
@@ -81,9 +89,9 @@ public class Mutant{
                 if(found > 1) return true;
                 missingChars = seqLength - newSeq.length();
                 // increase coord
-                if (direction.equals("1"))
+                if (direction == Direction.LEFT_RIGHT)
                     b++;
-                if (direction.equals("2"))
+                if (direction == Direction.RIGHT_LEFT)
                     b--;
             }
 
@@ -92,14 +100,14 @@ public class Mutant{
         int i = dna.length - 1; // last row
         int j = 1;
 
-        if (direction.equals("2"))
+        if (direction == Direction.RIGHT_LEFT)
             j = dna.length - 2;
-        while ((direction.equals("1") && j + missingChars <= dna.length)
-                || (direction.equals("2") && j - missingChars >= -1)) {
+        while ((direction == Direction.LEFT_RIGHT && j + missingChars <= dna.length)
+                || (direction == Direction.RIGHT_LEFT && j - missingChars >= -1)) {
             newSeq = "";
 
-            for (int a = i, b = j; (direction.equals("1") && b + missingChars <= dna.length)
-                    || (direction.equals("2") && b - missingChars >= -1); a--) {
+            for (int a = i, b = j; (direction == Direction.LEFT_RIGHT && b + missingChars <= dna.length)
+                    || (direction == Direction.RIGHT_LEFT && b - missingChars >= -1); a--) {
   
                 currentChar = dna[a].charAt(b);
                 nextChar = getNextChar(direction,dna,a,b);
@@ -107,23 +115,23 @@ public class Mutant{
                 if(found > 1) return true;
 
                 missingChars = seqLength - newSeq.length();
-                if (direction.equals("1"))
+                if (direction == Direction.LEFT_RIGHT)
                     b++;
-                if (direction.equals("2"))
+                if (direction == Direction.RIGHT_LEFT)
                     b--;
             }
-            if (direction.equals("1"))
+            if (direction == Direction.LEFT_RIGHT)
                 j++;
-            if (direction.equals("2"))
+            if (direction == Direction.RIGHT_LEFT)
                 j--;
         }
         return false;
     }
 
-    private static char getNextChar(String direction, String[] dna, int a, int b) {
-        if (direction.equals("1"))
+    private static char getNextChar(Direction direction, String[] dna, int a, int b) {
+        if (direction == Direction.LEFT_RIGHT)
          return dna[a - 1].charAt(b + 1);
-        if (direction.equals("2"))
+        if (direction == Direction.RIGHT_LEFT)
         return dna[a - 1].charAt(b - 1);
 
         return '.';
